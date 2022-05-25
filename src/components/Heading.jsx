4b08a9de5d994095ccee400ae971ff75e7e993a2
Dheerendra Panwar <Dhee2211@users.noreply.github.com>
2022-05-24 18:13:34 -0700

    Initial commit

---
+++ src/components/Heading.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+function Heading() {
+  return <h1 contentEditable>My favourite Food</h1>;
+}
+export default Heading;
---
+++ src/components/List.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { createPortal } from "react-dom";
+
+function List() {
+  return (
+    <ul>
+      <li>Lassi</li>
+      <li>Samosa</li>
+      <li>Panipuri</li>
+    </ul>
+  );
+}
+
+export default List;
